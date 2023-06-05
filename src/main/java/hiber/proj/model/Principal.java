eb60d02b3c709c1e7669922c558f5743fdc672f3
thalassa_home <blinov.gosha@mail.ru>
2023-06-05 19:49:24 +0300

    models (done)

--- src/main/java/hiber/proj/model/Principal.java
+++ src/main/java/hiber/proj/model/Principal.java
@@ -1,4 +1,71 @@
 package hiber.proj.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Principal")
 public class Principal {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "age")
+    int age;
+
+    @OneToOne(mappedBy = "principal")
+    private School school;
+
+    public Principal() {
+    }
+
+    public Principal(String name, int age, School school) {
+        this.name = name;
+        this.age = age;
+        this.school = school;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Principal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/hiber/proj/model/School.java
+++ src/main/java/hiber/proj/model/School.java
@@ -1,4 +1,60 @@
 package hiber.proj.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "School")
 public class School {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "school_number")
+    String name;
+
+    @OneToOne
+    @JoinColumn(name = "principal_id", referencedColumnName = "id")
+    private Principal principal;
+
+    public School() {
+    }
+
+    public School(String name, Principal principal) {
+        this.name = name;
+        this.principal = principal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Principal getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(Principal principal) {
+        this.principal = principal;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
+
